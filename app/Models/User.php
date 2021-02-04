2b83b5a24b14b9cef321017e0f3069d2b5d06878
vagrant <vagrant@homestead>
2021-02-04 08:23:22 +0000

    微博数据模型

--- app/Models/User.php
+++ app/Models/User.php
@@ -49,6 +49,12 @@ class User extends Authenticatable
     }
 
 
+    public function statuses()
+    {
+        return $this->hasMany(Status::class);
+    }
+
+
     public function gravatar($size = 100)
     {
         $hash = md5(strtolower(trim($this->attributes['email'])));
